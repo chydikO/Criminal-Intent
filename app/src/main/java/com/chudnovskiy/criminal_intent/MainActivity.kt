b3c131463ed27bf6963b315f55ade7899fb7998a
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-02-13 06:55:20 +0200

    add Fragment View

---
+++ app/src/main/java/com/chudnovskiy/criminal_intent/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.chudnovskiy.criminal_intent
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (currentFragment == null) {
+            val fragment = CrimeFragment()
+            supportFragmentManager.beginTransaction().add(R.id.fragment_container,fragment).commit()
+        }
+    }
+}
